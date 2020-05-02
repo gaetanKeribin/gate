302d5fb9369f8cc4782e74345678cf1f47c1df5e
gaetan <gaetan.keribin@gmail.com>
2020-05-02 21:23:40 +0200

    new build

--- client/web-build/register-service-worker.js
+++ client/web-build/register-service-worker.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker
-      .register('/expo-service-worker.js', { scope: '/' })
+      .register('/siee-gate-web/expo-service-worker.js', { scope: '/siee-gate-web/' })
       .then(function(info) {
         // console.info('Registered service-worker', info);
       })
