2ef93d815de32055b1a07fe5611b38978c621e60
gaetan <gaetan.keribin@gmail.com>
2020-05-04 18:37:47 +0200

    fixed api url

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export const apiConfig = {
-  baseUrl: "https://siee-gate.herokuapp.com/api/",
+  baseUrl: "https://siee-gate-test.herokuapp.com/api/",
   clientId: "1",
   clientSecret: "xc34jamesDevV41XwKbWhrsGgHvR3hjwG8",
 };
