6fd6dc81320124ab4446cfd204f0d5995f443f87
gaetan <gaetan.keribin@gmail.com>
2020-05-07 00:01:16 +0200

    minor

--- models/user.js
+++ models/user.js
@@ -104,15 +104,6 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
-userSchema.path("email").validate(function (value, done) {
-  this.model("User").count({ email: value }, function (err, count) {
-    if (err) {
-      return done(err);
-    }
-    done(!count);
-  });
-}, "Email already exists");
-
 userSchema.methods.toJSON = function () {
   const user = this;
   const userObject = user.toObject();
